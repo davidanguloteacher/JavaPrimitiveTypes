ab728dddb50a2555936cbe5eaa5f7222c8bab532
David <davidangulocorcuera@gmail.com>
2019-09-11 11:26:02 +0200

    first commit

---
+++ src/TypeExamples.java
@@ -0,0 +1,18 @@
+public class TypeExamples {
+    public static void main(String[] args) {
+
+        // print by console
+        // You can write the word sout and press "enter" for generate this code.
+        // System.out.println();
+
+        byte thisIsAByte = 5;
+        short thisIsAShort = 5;
+        int thisIsAInt = 5;
+        long thisIsALong = 5L;
+        float thisIsAFloat = 5.0F;
+        double thisIsADouble = 5.0;
+        char thisIsAChar = '5';
+        boolean isBoolean = true;
+        String thisIsAString = "5";
+    }
+}
